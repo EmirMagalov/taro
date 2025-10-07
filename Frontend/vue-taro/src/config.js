255a1cd3545aa0ef7991075e78ede4e106fdb317
Emir Magalov <emir.mahalo@gmail.com>
2025-10-07 15:40:55 +0400

    first commit

--- Frontend/vue-taro/src/config.js
+++ Frontend/vue-taro/src/config.js
@@ -1,4 +1,5 @@
 // export const url = 'http://127.0.0.1:8000/api/taro/'
-export const url = 'http://192.168.1.68:8000/api/taro/'
+// export const url = 'http://192.168.1.68:8000/api/taro/'
+export const url = 'https://flexado.xyz/api/taro/'
 
 
